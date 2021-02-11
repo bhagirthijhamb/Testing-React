d03ed9f8542a686ed3befdf7f8e6a27529394a91
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-11 16:31:14 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommentBox from './CommentBox';
+import ComentBox from './CommentBox';
+import CommentList from './CommentList';
+
+export default () => {
+  return (
+    <div>
+      I am the app component
+      <CommentBox />
+      <CommentList />
+    </div>
+  )
+}
