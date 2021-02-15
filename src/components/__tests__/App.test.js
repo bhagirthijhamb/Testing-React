149622107dbfdddd9ca85c5b79a59f50d0f22a3f
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-15 11:25:39 -0500

    Code Reuse with BeforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -23,13 +23,19 @@ import CommentList from 'components/CommentList';
 //   ReactDOM.unmountComponentAtNode(div);
 // })
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+})
+
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+  // const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 })
 
 it('show a comment list', () => {
-  const wrapped = shallow(<App />);
+  // const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 })
 
