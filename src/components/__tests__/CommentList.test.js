02c2ebff02d86df9dc1526baedbee293bed222e8
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-17 10:22:23 -0500

    Cheerio Queries

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -18,5 +18,12 @@ beforeEach(() => {
 })
 
 it('creates one li per comment', () => {
-  console.log(wrapped.find('li').length);
+  // console.log(wrapped.find('li').length);
+  expect(wrapped.find('li').length).toEqual(2);
+})
+
+it('shows the text for each comment', () => {
+  // console.log(wrapped.render().text());
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
 })
