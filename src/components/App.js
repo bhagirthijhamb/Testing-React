7d96435c08b66b7ac85ea90a84bfc0a2577aab43
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-18 09:13:42 -0500

    Rendering Header, wiring up state, Changing Auth State

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      {/* I am the app component
-      <CommentBox />
-      <CommentList /> */}
+class App extends React.Component {
+  renderButton(){
+    if(this.props.auth){
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          Sign Out
+        </button>
+      )
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>
+          Sign In
+        </button>
+      )
+    }
+  }
+  renderHeader(){
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
 
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  )
+  render (){
+    return (
+      <div>
+        {/* I am the app component
+        <CommentBox />
+        <CommentList /> */}
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    )
+  }
 }
+
+function mapStateToProps(state){
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
